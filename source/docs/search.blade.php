b8093fd0513c8c969aef28c5d7106c255cf32a84
rockhopper72 <github2@steven.globaloffset.com>
2019-03-20 09:43:46 -0400

    Autocomplete can be closed on the search page

--- source/docs/search.blade.php
+++ source/docs/search.blade.php
@@ -4,18 +4,16 @@
 
 @push('scripts')
 <script type="text/javascript">
-    (function(){
+    $.when($.ready).then(function() {
         document.addEventListener('DOMContentLoaded', function() {
             const urlParams = new URLSearchParams(window.location.search);
             const query = urlParams.get('q');
+            
             const docsearchAutocomplete = search.autocomplete; // The underlying autocomplete.js instance
             const autocomplete = docsearchAutocomplete.autocomplete; 
 
             autocomplete.setVal(query);
             autocomplete.open();
-            docsearchAutocomplete.on('autocomplete:closed', (event) => {
-                autocomplete.open();
-            });
         });
     }());
 </script>
