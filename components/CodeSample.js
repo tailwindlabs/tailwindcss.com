beb5023c06acc658a4fd01cf419e3ceb1f094f61
Adam Wathan <adam.wathan@gmail.com>
2019-08-05 09:04:57 -0400

    Extract tip components

--- components/CodeSample.js
+++ components/CodeSample.js
@@ -1,14 +1,9 @@
-export default ({ children, code, bg = 'bg-white', px = 'px-4', py = 'py-4' }) => (
+export default ({ preview, code, bg = 'bg-white', px = 'px-4', py = 'py-4' }) => (
   <div className="relative overflow-hidden mt-6 mb-8">
-    {children
-      ? <div className={`rounded-t-lg overflow-hidden border-t border-l border-r border-gray-400 ${bg} ${px} ${py}`}>
-          {children}
-        </div>
-      : <div
-          className="bg-white rounded-t-lg overflow-hidden border-t border-l border-r border-gray-400 p-4"
-          dangerouslySetInnerHTML={{ __html: code }}
-        />
-    }
+    <div
+      className={`rounded-t-lg overflow-hidden border-t border-l border-r border-gray-400 ${bg} ${px} ${py}`}
+      dangerouslySetInnerHTML={{ __html: preview || code }}
+    />
     <div className="rounded-b-lg bg-gray-800">
       <pre className="scrollbar-none m-0 p-0 language-html"><code className="inline-block p-4 scrolling-touch">{code}</code></pre>
     </div>
