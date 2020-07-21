2d85347bc523921d7351409e1949953cd99f09d6
Brad Cornes <bradlc41@gmail.com>
2020-07-21 12:40:57 +0100

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const withMDX = require('@next/mdx')()
-
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'mdx'],
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 })
+
+module.exports = withBundleAnalyzer(
+  withMDX({
+    pageExtensions: ['js', 'jsx', 'mdx'],
+  })
+)
