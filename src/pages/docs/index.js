60a1a5e69f1c8ed1a17b45fdf929ecd260656b4a
Simon Vrachliotis <simon@simonswiss.com>
2021-06-18 09:32:19 +1000

    longer excerpt matching the blog post

--- src/pages/docs/index.js
+++ src/pages/docs/index.js
@@ -66,8 +66,9 @@ const latestUpdates = [
     title: 'Tailwind CSS v2.2',
     date: '2021-06-17T19:00:00.000Z',
     url: 'https://blog.tailwindcss.com/tailwindcss-2-2',
-    description:
-      "We've built-in a new high-performance CLI tool, added before and after pseudo-element support, introduced new sibling selector variants, and tons more.",
+    description: `Over the last few weeks, we've been having a ton of fun dumping new and exciting features into Tailwind. Now feels like the right time to cut a release, so here it is — Tailwind CSS v2.2!
+
+      We've built-in a new high-performance CLI tool, added before and after pseudo-element support, introduced new sibling selectors, variants for styling highlighted text, and tons more.`,
   },
   {
     title: 'Tailwind CSS v2.1',
