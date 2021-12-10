2189eaecf5dff6b3d3cdc7003319f88905c82c28
Brad Cornes <bradlc41@gmail.com>
2021-12-10 10:24:31 +0000

    Fix icon cut off

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,11 +29,11 @@ function NpmInstallButton() {
   return (
     <button
       type="button"
-      className="w-full sm:w-auto flex-none bg-gray-50 text-gray-400 hover:text-gray-900 font-mono leading-6 py-3 sm:px-6 border border-gray-200 rounded-xl flex items-center justify-center space-x-2 sm:space-x-4 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-300 focus:outline-none transition-colors duration-200"
+      className="flex items-center justify-center flex-none w-full py-3 space-x-2 font-mono leading-6 text-gray-400 transition-colors duration-200 border border-gray-200 sm:w-auto bg-gray-50 hover:text-gray-900 sm:px-6 rounded-xl sm:space-x-4 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-300 focus:outline-none"
       onClick={() => copy()}
     >
       <span className="text-gray-900">
-        <span className="hidden sm:inline text-gray-500" aria-hidden="true">
+        <span className="hidden text-gray-500 sm:inline" aria-hidden="true">
           ${' '}
         </span>
         npm install tailwindcss
@@ -48,7 +48,7 @@ function NpmInstallButton() {
 
 export default function Home() {
   return (
-    <div className="space-y-20 sm:space-y-32 md:space-y-40 lg:space-y-44 overflow-hidden">
+    <>
       <Head>
         <meta
           key="twitter:title"
@@ -62,64 +62,19 @@ export default function Home() {
         />
         <title>Tailwind CSS - Rapidly build modern websites without ever leaving your HTML.</title>
       </Head>
-      <header className="relative z-10 max-w-screen-lg xl:max-w-screen-xl mx-auto">
-        <div className="px-4 sm:px-6 md:px-8 mb-14 sm:mb-20 xl:mb-8">
-          <div className="border-b border-gray-200 py-4 flex items-center justify-between mb-16 sm:mb-20 -mx-4 px-4 sm:mx-0 sm:px-0">
-            <Search />
-            <div className="flex items-center space-x-6 sm:space-x-10 ml-6 sm:ml-10">
-              <NextLink href="/docs">
-                <a className="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2">
-                  <span className="sm:hidden">Docs</span>
-                  <span className="hidden sm:inline">Documentation</span>
-                </a>
-              </NextLink>
-              <a
-                href="https://github.com/tailwindlabs/tailwindcss"
-                className="text-gray-400 hover:text-gray-500 transition-colors duration-200"
-              >
-                <span className="sr-only">Tailwind CSS on GitHub</span>
-                <svg width="24" height="24" viewBox="0 0 16 16" fill="currentColor">
-                  <path
-                    fillRule="evenodd"
-                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-          <Logo className="w-auto h-7 sm:h-8" />
-          <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-8 sm:mt-14 sm:mb-10">
-            Rapidly build modern websites without ever leaving your HTML.
-          </h1>
-          <p className="max-w-screen-lg text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">
-            A utility-first CSS framework packed with classes like <InlineCode>flex</InlineCode>,{' '}
-            <InlineCode>pt-4</InlineCode>, <InlineCode>text-center</InlineCode> and{' '}
-            <InlineCode>rotate-90</InlineCode> that can be composed to build any design, directly in
-            your markup.
-          </p>
-          <div className="flex flex-wrap space-y-4 sm:space-y-0 sm:space-x-4 text-center">
-            <NextLink href="/docs">
-              <a className="w-full sm:w-auto flex-none bg-gray-900 hover:bg-gray-700 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">
-                Get started
-              </a>
-            </NextLink>
-            <NpmInstallButton />
-          </div>
-        </div>
-        <Hero />
-      </header>
-      <section className="relative z-10 text-center max-w-screen-lg xl:max-w-screen-xl mx-auto">
-        <div className="px-4 sm:px-6 md:px-8">
-          <BigText as="h2" className="mb-8">
-            <Widont>“Best practices” don’t actually work.</Widont>
-          </BigText>
+      <div className="mb-20 space-y-20 overflow-hidden sm:mb-32 sm:space-y-32 md:mb-40 md:space-y-40">
+        <Header />
+        <section className="px-8 text-center">
+          <h2 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
+            “Best practices” don’t actually work.
+          </h2>
           <figure>
             <blockquote>
               <Paragraph className="max-w-4xl mx-auto mb-6">
                 I’ve written{' '}
                 <a
                   href="https://adamwathan.me/css-utility-classes-and-separation-of-concerns/"
-                  className="text-light-blue-600 font-semibold"
+                  className="font-semibold text-light-blue-600"
                   style={{
                     boxShadow:
                       'inset 0 -0.1666666667em 0 0 #fff, inset 0 -0.3333333333em 0 0 #bae6fd',
@@ -135,8 +90,8 @@ export default function Home() {
                 </Widont>
               </Paragraph>
             </blockquote>
-            <figcaption className="sm:text-xl font-medium flex flex-col items-center">
-              <div className="p-1 border-2 border-light-blue-400 rounded-full mb-3">
+            <figcaption className="flex flex-col items-center font-medium sm:text-xl">
+              <div className="p-1 mb-3 border-2 rounded-full border-light-blue-400">
                 <img
                   src={require('@/img/adam.jpg').default}
                   alt=""
@@ -151,7 +106,7 @@ export default function Home() {
         </div>
       </section>
       <Testimonials />
-      <div className="max-w-screen-lg xl:max-w-screen-xl mx-auto space-y-20 sm:space-y-32 md:space-y-40 lg:space-y-44">
+      <div className="pt-20 mb-20 space-y-20 overflow-hidden sm:pt-32 sm:mb-32 sm:space-y-32 md:pt-40 md:mb-40 md:space-y-40">
         <ConstraintBased />
         <BuildAnything />
         <Performance />
@@ -165,6 +120,6 @@ export default function Home() {
         <ReadyMadeComponents />
       </div>
       <Footer />
-    </div>
+    </>
   )
 }
