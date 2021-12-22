7adba724db763da974529ca0911ab18b898e9569
David <davidisrawi@gmail.com>
2021-12-22 17:42:31 -0500

    svg titles added: a11y

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -45,6 +45,7 @@ function SunIcon({ selected, ...props }) {
       strokeLinejoin="round"
       {...props}
     >
+      <title>{label}</title>
       <path
         d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
         className={
@@ -62,6 +63,7 @@ function SunIcon({ selected, ...props }) {
 function MoonIcon({ selected, ...props }) {
   return (
     <svg viewBox="0 0 24 24" fill="none" {...props}>
+      <title>{label}</title>
       <path
         fillRule="evenodd"
         clipRule="evenodd"
@@ -85,6 +87,7 @@ function MoonIcon({ selected, ...props }) {
 function PcIcon({ selected, ...props }) {
   return (
     <svg viewBox="0 0 24 24" fill="none" {...props}>
+      <title>{label}</title>
       <path
         d="M4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6Z"
         strokeWidth="2"
