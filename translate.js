021d424a33a12ee4560c04920e1cabffaedb2d86
Augustine <ashramwen@users.noreply.github.com>
2022-01-17 09:44:13 +0800

    testing (#150)
    * add file_replacer
    
    * add base-dir support and pathType checking
    
    * test translated files
    
    * test translated files
    
    * Using lower-capital-camelcase, Using fs promises api
    
    * Using path module , Adding catch statements
    
    * fix coding style
    
    * fix typo, fix coding style
    
    * tester.js renamed to file-replacer.js
    
    Co-authored-by: 搋兔 <naruto6404@hotmail.com>

--- translate.js
+++ translate.js
@@ -8,5 +8,34 @@ module.exports = () => {
         replace: "元件",
       },
     },
+    {
+      test: /\.mdx$/,
+      loader: "./file-replacer",
+      options: {
+        base: "./translated",
+        replacement: {
+          "editor-setup.mdx": "editor-setup.mdx",
+        },
+      },
+    },
+    {
+      test: /documentation\.js$/,
+      loader: "./file-replacer",
+      options: {
+        replacement: {
+          "documentation.js": "./translated/navs/documentation.js",
+        },
+      },
+    },
+    {
+      test: /components[\/+\\+]home[\/+\\+]\w*\.js$/,
+      loader: "./file-replacer",
+      options: {
+        base: "./translated/components/home",
+        replacement: {
+          "Hero.js": "Hero.js",
+        },
+      },
+    },
   ];
 };
