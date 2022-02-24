72bd7053756ae8f94fabc18d4847afd0ddcd3793
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-02-25 00:17:01 +0200

    fix: remove computed font size

--- src/utils/previewRemToPx.jsx
+++ src/utils/previewRemToPx.jsx
@@ -1,14 +1,9 @@
 export function previewRemToPx(css, { className }) {
   return (
     <td className={`text-right leading-6 font-mono text-xs whitespace-pre ${className}`}>
-      {Object.entries(css).filter(([_, v]) => v.endsWith('rem')).map(([k, v]) => {
-        // TODO: html font size, get it from some storage, defaults to 16
-        const fontSize = typeof window !== 'undefined'
-          ? window.getComputedStyle(document.documentElement).fontSize
-          : '16px'
-        const rootFontSize = Number.parseFloat(fontSize)
-        const rem = Number.parseFloat(v)
-        return <div className="px-6" key={k}>{rem * rootFontSize}px</div>
+      {Object.entries(css).map(([k, v]) => {
+        const px = v.endsWith('rem') ? `${Number.parseFloat(v) * 16}px` : undefined
+        return <div className="px-6" key={k}>{px}</div>
       })}
     </td>
   )
