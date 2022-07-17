37f30fa46140a8a7bbc76d89ea7ed6063bc64d59
kdxcxs <cx@kdxcxs.com>
2022-07-17 15:25:11 +0800

    Add `svelte` template into `tailwind.config.js`

--- src/pages/docs/guides/vite.js
+++ src/pages/docs/guides/vite.js
@@ -48,7 +48,7 @@ let steps = [
       code: `  module.exports = {
 >   content: [
 >     "./index.html",
->     "./src/**/*.{vue,js,ts,jsx,tsx}",
+>     "./src/**/*.{vue,js,ts,jsx,tsx,svelte}",
 >   ],
     theme: {
       extend: {},
