94d7cd8267d41cd73847d71e9269bebb8f9e9576
babakfp <44144724+babakfp@users.noreply.github.com>
2022-09-09 11:36:50 -0700

    Filled space between links with padding

--- src/layouts/SidebarLayout.js
+++ src/layouts/SidebarLayout.js
@@ -13,7 +13,7 @@ const NavItem = forwardRef(({ href, children, isActive, isPublished, fallbackHre
     <li ref={ref}>
       <Link href={isPublished ? href : fallbackHref}>
         <a
-          className={clsx('block border-l pl-4 -ml-px', {
+          className={clsx('block border-l pl-4 py-2 -ml-px lg:py-1', {
             'text-sky-500 border-current font-semibold dark:text-sky-400': isActive,
             'border-transparent hover:border-slate-400 dark:hover:border-slate-500': !isActive,
             'text-slate-700 hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300':
@@ -159,7 +159,7 @@ function Nav({ nav, children, fallbackHref, mobile = false }) {
                   </h5>
                   <ul
                     className={clsx(
-                      'space-y-6 lg:space-y-2 border-l border-slate-100',
+                      'border-l border-slate-100',
                       mobile ? 'dark:border-slate-700' : 'dark:border-slate-800'
                     )}
                   >
