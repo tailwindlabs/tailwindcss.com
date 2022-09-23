46c32a014d169136637c7d18d88c7f85b3cebbf7
steve <sdmcallister@gmail.com>
2022-09-23 17:30:26 -0400

    add qwik to vite section

--- src/pages/docs/guides/vite.js
+++ src/pages/docs/guides/vite.js
@@ -212,7 +212,101 @@ let tabs = [
         },
       },
     ],
-  },
+  }, {
+    name: 'Using Qwik',
+    href: '#qwik',
+    steps: [
+      {
+        title: 'Create your project',
+        body: () => (
+          <p>
+            Start by creating a new Qwik project if you don’t have one set up already.
+          </p>
+        ),
+        code: {
+          name: 'Terminal',
+          lang: 'terminal',
+          code: 'npm create qwik@latest',
+        },
+      },
+      {
+        title: 'Install Tailwind CSS',
+        body: () => (
+          <p>
+            Install <code>tailwindcss</code> and its peer dependencies via npm, and then run the
+            init command to generate both <code>tailwind.config.js</code> and{' '}
+            <code>postcss.config.js</code>.
+          </p>
+        ),
+        code: {
+          name: 'Terminal',
+          lang: 'terminal',
+          code: 'npm install -D tailwindcss postcss autoprefixer\nnpx tailwindcss init -p',
+        },
+      },
+      {
+        title: 'Configure your template paths',
+        body: () => (
+          <p>
+            Add the paths to all of your template files in your <code>tailwind.config.js</code>{' '}
+            file.
+          </p>
+        ),
+        code: {
+          name: 'tailwind.config.js',
+          lang: 'js',
+          code: `  /** @type {import('tailwindcss').Config} */
+  module.exports = {
+>   content: [
+>     "./src/**/*.{js,ts,jsx,tsx}",
+>   ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }`,
+        },
+      },
+      {
+        title: 'Add the Tailwind directives to your CSS',
+        body: () => (
+          <p>
+            Add the <code>@tailwind</code> directives for each of Tailwind’s layers to your{' '}
+            <code>./src/Global.css</code> file.
+          </p>
+        ),
+        code: {
+          name: 'Global.css',
+          lang: 'css',
+          code: '@tailwind base;\n@tailwind components;\n@tailwind utilities;',
+        },
+      },
+      {
+        title: 'Start your build process',
+        body: () => (
+          <p>
+            Run your build process with <code>npm run dev</code>.
+          </p>
+        ),
+        code: {
+          name: 'Terminal',
+          lang: 'terminal',
+          code: 'npm run dev',
+        },
+      },
+      {
+        title: 'Start using Tailwind in your project',
+        body: () => <p>Start using Tailwind’s utility classes to style your content.</p>,
+        code: {
+          name: 'index.tsx',
+          lang: 'tsx',
+          code: `  export const App = component$(() => {
+    return <p class="text-3xl font-bold underline">Hello Qwik</p>;
+> });`,
+        },
+      },
+    ],
+  }
 ]
 
 export default function UsingVite({ code }) {
