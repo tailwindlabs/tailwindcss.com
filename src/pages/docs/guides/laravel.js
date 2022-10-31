a313713e3a0cfe84cfcacc813d55bd9dbfe5dede
Marcus Förster <git@xerc.de>
2022-10-31 16:55:40 +0100

    Fix `laravel` guide @ blade integr.
    w/o active `vite` ; "Unable to locate file in Vite manifest: resources/css/app.css."

--- src/pages/docs/guides/laravel.js
+++ src/pages/docs/guides/laravel.js
@@ -112,7 +112,7 @@ let tabs = [
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
   </head>
   <body>
 >   <h1 class="text-3xl font-bold underline">
