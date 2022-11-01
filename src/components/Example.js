d0d296b69e2b2abb471e7508e0ef6a1cb084e09c
Brad Cornes <hello@bradley.dev>
2022-11-01 12:29:34 +0000

    Hide resize hint on smaller screen sizes

--- src/components/Example.js
+++ src/components/Example.js
@@ -14,6 +14,7 @@ function Well({
   html,
   children,
   hint,
+  hintClassName,
   lightOnly = false,
 }) {
   let paddingKey = padding ?? p
@@ -26,26 +27,28 @@ function Well({
   return (
     <div className={containerClassName}>
       {hint !== undefined && (
-        <div className="not-prose mb-4 flex space-x-2">
-          <svg
-            className="flex-none w-5 h-5"
-            viewBox="0 0 20 20"
-            fill="none"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            aria-hidden="true"
-          >
-            <path
-              d="m9.813 9.25.346-5.138a1.276 1.276 0 0 0-2.54-.235L6.75 11.25 5.147 9.327a1.605 1.605 0 0 0-2.388-.085.018.018 0 0 0-.004.019l1.98 4.87a5 5 0 0 0 4.631 3.119h3.885a4 4 0 0 0 4-4v-1a3 3 0 0 0-3-3H9.813Z"
-              className="stroke-slate-400 dark:stroke-slate-300"
-            />
-            <path
-              d="M3 5s.35-.47 1.25-.828m9.516-.422c2.078.593 3.484 1.5 3.484 1.5"
-              className="stroke-slate-400 dark:stroke-sky-400"
-            />
-          </svg>
-          <p className="text-slate-700 text-sm font-medium dark:text-slate-200">{hint}</p>
+        <div className={clsx(hintClassName, 'not-prose mb-4')}>
+          <div className="flex space-x-2">
+            <svg
+              className="flex-none w-5 h-5"
+              viewBox="0 0 20 20"
+              fill="none"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              aria-hidden="true"
+            >
+              <path
+                d="m9.813 9.25.346-5.138a1.276 1.276 0 0 0-2.54-.235L6.75 11.25 5.147 9.327a1.605 1.605 0 0 0-2.388-.085.018.018 0 0 0-.004.019l1.98 4.87a5 5 0 0 0 4.631 3.119h3.885a4 4 0 0 0 4-4v-1a3 3 0 0 0-3-3H9.813Z"
+                className="stroke-slate-400 dark:stroke-slate-300"
+              />
+              <path
+                d="M3 5s.35-.47 1.25-.828m9.516-.422c2.078.593 3.484 1.5 3.484 1.5"
+                className="stroke-slate-400 dark:stroke-sky-400"
+              />
+            </svg>
+            <p className="text-slate-700 text-sm font-medium dark:text-slate-200">{hint}</p>
+          </div>
         </div>
       )}
       <Component
