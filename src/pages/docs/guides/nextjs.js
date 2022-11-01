daf1539bd646a607d13a1565c498035eeba117d1
Tim Neutkens <tim@timneutkens.nl>
2022-11-01 14:08:15 +0100

    Add `app` directory to content for Next.js (#1389)
    Ensures tailwind is compatible with https://nextjs.org/blog/layouts-rfc when it becomes available.

--- src/pages/docs/guides/nextjs.js
+++ src/pages/docs/guides/nextjs.js
@@ -45,6 +45,7 @@ let steps = [
       code: `  /** @type {import('tailwindcss').Config} */
   module.exports = {
 >   content: [
+>     "./app/**/*.{js,ts,jsx,tsx}",
 >     "./pages/**/*.{js,ts,jsx,tsx}",
 >     "./components/**/*.{js,ts,jsx,tsx}",
 >   ],
