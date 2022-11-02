ee5ed0173309786083ae098a3b3d5f7c2f47d713
Brad Cornes <hello@bradley.dev>
2022-11-02 18:14:43 +0000

    Remove "social square" image

--- src/layouts/DocumentationLayout.js
+++ src/layouts/DocumentationLayout.js
@@ -1,6 +1,4 @@
 import { SidebarLayout } from '@/layouts/SidebarLayout'
-import Head from 'next/head'
-import socialSquare from '@/img/social-square.jpg'
 import { Title } from '@/components/Meta'
 import { documentationNav } from '@/navs/documentation'
 
@@ -8,14 +6,6 @@ export function DocumentationLayout(props) {
   return (
     <>
       <Title>{props.layoutProps.meta.metaTitle || props.layoutProps.meta.title}</Title>
-      <Head>
-        <meta key="twitter:card" name="twitter:card" content="summary" />
-        <meta
-          key="twitter:image"
-          name="twitter:image"
-          content={`https://tailwindcss.com${socialSquare.src}`}
-        />
-      </Head>
       <SidebarLayout nav={documentationNav} {...props} />
     </>
   )
