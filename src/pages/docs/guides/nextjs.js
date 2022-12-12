9ebc2b47472a6c00716a9d1f065c6a3ea0946c65
Thiago de Bastos <6151341+thiagodebastos@users.noreply.github.com>
2022-12-12 15:59:28 +1100

    Update guide for appDir feature
    Following the instructions does not work if using NextJS's experimental `appDir` feature.

--- src/pages/docs/guides/nextjs.js
+++ src/pages/docs/guides/nextjs.js
@@ -66,6 +66,7 @@ let tabs = [
 >   content: [
 >     "./pages/**/*.{js,ts,jsx,tsx}",
 >     "./components/**/*.{js,ts,jsx,tsx}",
+>     "./app/**/*.{js,ts,jsx,tsx}",
 >   ],
     theme: {
       extend: {},
