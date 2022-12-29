e06877b28c728a5f38cbeecce6ba77016b9d4522
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2022-12-28 21:59:18 -0500

    Emphasize discouragement of CRA more strongly

--- src/pages/docs/guides/create-react-app.js
+++ src/pages/docs/guides/create-react-app.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { DocumentationLayout } from '@/layouts/DocumentationLayout'
 import { FrameworkGuideLayout } from '@/layouts/FrameworkGuideLayout'
 import { Steps } from '@/components/Steps'
+import { Cta } from '@/components/Cta'
 
 let steps = [
   {
@@ -23,14 +24,14 @@ let steps = [
     title: 'Install Tailwind CSS',
     body: () => (
       <p>
-        Install <code>tailwindcss</code> and its peer dependencies via npm, and then run the init
-        command to generate both <code>tailwind.config.js</code> and <code>postcss.config.js</code>.
+        Install <code>tailwindcss</code> via npm, and then run the init command to generate your{' '}
+        <code>tailwind.config.js</code> file.
       </p>
     ),
     code: {
       name: 'Terminal',
       lang: 'terminal',
-      code: 'npm install -D tailwindcss postcss autoprefixer\nnpx tailwindcss init -p',
+      code: 'npm install -D tailwindcss\nnpx tailwindcss init',
     },
   },
   {
@@ -105,9 +106,9 @@ export default function UsingCRA({ code }) {
       title="Install Tailwind CSS with Create React App"
       description="Setting up Tailwind CSS in a Create React App project."
     >
-      <div className="relative z-10 prose prose-slate mb-16 max-w-3xl dark:prose-dark">
+      <div hidden className="relative z-10 prose prose-slate mb-16 max-w-3xl dark:prose-dark">
         <p>
-          Note that for new React projects, we highly recommend using{' '}
+          We <strong>highly recommend</strong> using{' '}
           <Link href="https://vitejs.dev">
             <a>Vite</a>
           </Link>
@@ -125,8 +126,54 @@ export default function UsingCRA({ code }) {
           </Link>{' '}
           instead of Create React App. They provide an equivalent or better developer experience but
           with more flexibility, giving you more control over how Tailwind and PostCSS are
-          configured.
+          configured. Create React App does not support custom PostCSS configurations, so you can't
+          use
         </p>
+      </div>
+
+      <div className="mb-16 py-6 px-4 bg-amber-500/10 sm:p-6 lg:p-6 rounded-lg dark:bg-black/25 max-w-4xl">
+        <div className="flex">
+          <svg
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="fill-amber-900 w-5 h-5 shrink-0 mt-1 mr-4 dark:fill-slate-400"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
+              clipRule="evenodd"
+            />
+          </svg>
+
+          <div className="text-sm leading-6 text-amber-900 dark:text-slate-400">
+            <p className="font-bold dark:text-slate-200">
+              Create React App does not support custom PostCSS configurations and is incompatible
+              with many important tools in the PostCSS ecosystem, like{' '}
+              <code className="text-xs font-bold">`postcss-import`</code>.
+            </p>
+            <p className="mt-3">
+              We <strong className="dark:text-slate-200">highly recommend</strong> using{' '}
+              <Link href="/docs/guides/vite">
+                <a className="underline font-bold dark:text-slate-200">Vite</a>
+              </Link>
+              ,{' '}
+              <Link href="/docs/guides/parcel">
+                <a className="underline font-bold dark:text-slate-200">Parcel</a>
+              </Link>
+              ,{' '}
+              <Link href="/docs/guides/nextjs">
+                <a className="underline font-bold dark:text-slate-200">Next.js</a>
+              </Link>
+              , or{' '}
+              <Link href="/docs/guides/remix">
+                <a className="underline font-bold dark:text-slate-200">Remix</a>
+              </Link>{' '}
+              instead of Create React App. They provide an equivalent or better developer experience
+              but with more flexibility, giving you more control over how Tailwind and PostCSS are
+              configured.
+            </p>
+          </div>
+        </div>
       </div>
       <Steps steps={steps} code={code} />
     </FrameworkGuideLayout>
