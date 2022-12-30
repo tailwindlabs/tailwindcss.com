1d354aed040aa55aca802fc32ac406666f765f05
Kirill <87936030+MisterKirill@users.noreply.github.com>
2022-12-30 16:43:12 +0500

    Update NextJS integration guide
    Add "./app/**/*.{js,ts,jsx,tsx}" as mentioned at https://beta.nextjs.org/docs/styling/tailwind-css#configuring-tailwind

--- src/pages/docs/guides/nextjs.js
+++ src/pages/docs/guides/nextjs.js
@@ -64,6 +64,7 @@ let tabs = [
           code: `  /** @type {import('tailwindcss').Config} */
   module.exports = {
 >   content: [
+>     "./app/**/*.{js,ts,jsx,tsx}",
 >     "./pages/**/*.{js,ts,jsx,tsx}",
 >     "./components/**/*.{js,ts,jsx,tsx}",
 >   ],
