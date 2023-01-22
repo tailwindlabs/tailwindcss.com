f5488d58381a5708d8b3ddc63f15f987ac3fbdec
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2023-01-22 13:19:19 -0500

    Simplify Next.js framework guide

--- src/pages/docs/guides/nextjs.js
+++ src/pages/docs/guides/nextjs.js
@@ -1,193 +1,48 @@
-import { useRouteHash } from '@/hooks/useRouteHash'
 import { DocumentationLayout } from '@/layouts/DocumentationLayout'
 import { FrameworkGuideLayout } from '@/layouts/FrameworkGuideLayout'
 import { Steps } from '@/components/Steps'
-import { TabBar } from '@/components/Guides/TabBar.jsx'
 
-let tabs = [
+let steps = [
   {
-    name: 'Stable',
-    key: 'stable',
-    href: '#',
-    intro: () => (
+    title: 'Create your project',
+    body: () => (
       <p>
-        The quickest way to start using Tailwind CSS in your Next.js project is to use the{' '}
-        <a href="https://github.com/vercel/next.js/tree/c3e5caf1109a2eb42801de23fc78e42a08e5da6e/examples/with-tailwindcss">
-          Next.js + Tailwind CSS Example
-        </a>
-        . This will automatically configure your Tailwind setup based on the official Next.js
-        example. If you'd like to configure Tailwind manually, continue with the rest of this guide.
+        Start by creating a new Next.js project if you don’t have one set up already. The most
+        common approach is to use{' '}
+        <a href="https://nextjs.org/docs/api-reference/create-next-app">Create Next App</a>.
       </p>
     ),
-    steps: [
-      {
-        title: 'Create your project',
-        body: () => (
-          <p>
-            Start by creating a new Next.js project if you don’t have one set up already. The most
-            common approach is to use{' '}
-            <a href="https://nextjs.org/docs/api-reference/create-next-app">Create Next App</a>.
-          </p>
-        ),
-        code: {
-          name: 'Terminal',
-          lang: 'terminal',
-          code: 'npx create-next-app@latest my-project --typescript --eslint\ncd my-project',
-        },
-      },
-      {
-        title: 'Install Tailwind CSS',
-        body: () => (
-          <p>
-            Install <code>tailwindcss</code> and its peer dependencies via npm, and then run the
-            init command to generate both <code>tailwind.config.js</code> and{' '}
-            <code>postcss.config.js</code>.
-          </p>
-        ),
-        code: {
-          name: 'Terminal',
-          lang: 'terminal',
-          code: 'npm install -D tailwindcss postcss autoprefixer\nnpx tailwindcss init -p',
-        },
-      },
-      {
-        title: 'Configure your template paths',
-        body: () => (
-          <p>
-            Add the paths to all of your template files in your <code>tailwind.config.js</code>{' '}
-            file.
-          </p>
-        ),
-        code: {
-          name: 'tailwind.config.js',
-          lang: 'js',
-          code: `  /** @type {import('tailwindcss').Config} */
-  module.exports = {
->   content: [
->     "./pages/**/*.{js,ts,jsx,tsx}",
->     "./components/**/*.{js,ts,jsx,tsx}",
-> ​
->     // Or if using \`src\` directory:
->     "./src/**/*.{js,ts,jsx,tsx}",
->   ],
-    theme: {
-      extend: {},
+    code: {
+      name: 'Terminal',
+      lang: 'terminal',
+      code: 'npx create-next-app@latest my-project --typescript --eslint\ncd my-project',
     },
-    plugins: [],
-  }`,
-        },
-      },
-      {
-        title: 'Add the Tailwind directives to your CSS',
-        body: () => (
-          <p>
-            Add the <code>@tailwind</code> directives for each of Tailwind’s layers to your{' '}
-            <code>./styles/globals.css</code> file.
-          </p>
-        ),
-        code: {
-          name: 'globals.css',
-          lang: 'css',
-          code: '@tailwind base;\n@tailwind components;\n@tailwind utilities;',
-        },
-      },
-      {
-        title: 'Start your build process',
-        body: () => (
-          <p>
-            Run your build process with <code>npm run dev</code>.
-          </p>
-        ),
-        code: {
-          name: 'Terminal',
-          lang: 'terminal',
-          code: 'npm run dev',
-        },
-      },
-      {
-        title: 'Start using Tailwind in your project',
-        body: () => <p>Start using Tailwind’s utility classes to style your content.</p>,
-        code: {
-          name: 'index.tsx',
-          lang: 'jsx',
-          code: `  export default function Home() {
-    return (
->     <h1 className="text-3xl font-bold underline">
->       Hello world!
->     </h1>
-    )
-  }`,
-        },
-      },
-    ],
   },
   {
-    name: (
-      <>
-        <span>
-          Using the{' '}
-          <code className="font-mono font-medium px-1 py-0.5 text-[13px] leading-5 rounded">
-            app
-          </code>{' '}
-          directory
-        </span>
-      </>
-    ),
-    key: 'app-directory',
-    href: '#app-directory',
-    intro: () => (
+    title: 'Install Tailwind CSS',
+    body: () => (
       <p>
-        If you’re beta testing the new{' '}
-        <a href="https://beta.nextjs.org/docs/routing/fundamentals#the-app-directory">
-          app directory
-        </a>{' '}
-        in Next.js 13, you’ll need to include the <code>./app</code> directory in your content paths
-        and ensure your CSS file is imported in your main layout file.
+        Install <code>tailwindcss</code> and its peer dependencies via npm, and then run the init
+        command to generate both <code>tailwind.config.js</code> and <code>postcss.config.js</code>.
       </p>
     ),
-    steps: [
-      {
-        title: 'Create your project',
-        body: () => (
-          <p>
-            Start by creating a new Next.js project if you don’t have one set up already. The most
-            common approach is to use{' '}
-            <a href="https://nextjs.org/docs/api-reference/create-next-app">Create Next App</a>.
-          </p>
-        ),
-        code: {
-          name: 'Terminal',
-          lang: 'terminal',
-          code: 'npx create-next-app@latest my-project --experimental-app --typescript --eslint\ncd my-project',
-        },
-      },
-      {
-        title: 'Install Tailwind CSS',
-        body: () => (
-          <p>
-            Install <code>tailwindcss</code> and its peer dependencies via npm, and then run the
-            init command to generate both <code>tailwind.config.js</code> and{' '}
-            <code>postcss.config.js</code>.
-          </p>
-        ),
-        code: {
-          name: 'Terminal',
-          lang: 'terminal',
-          code: 'npm install -D tailwindcss postcss autoprefixer\nnpx tailwindcss init -p',
-        },
-      },
-      {
-        title: 'Configure your template paths',
-        body: () => (
-          <p>
-            Add the paths to all of your template files in your <code>tailwind.config.js</code>{' '}
-            file.
-          </p>
-        ),
-        code: {
-          name: 'tailwind.config.js',
-          lang: 'js',
-          code: `  /** @type {import('tailwindcss').Config} */
+    code: {
+      name: 'Terminal',
+      lang: 'terminal',
+      code: 'npm install -D tailwindcss postcss autoprefixer\nnpx tailwindcss init -p',
+    },
+  },
+  {
+    title: 'Configure your template paths',
+    body: () => (
+      <p>
+        Add the paths to all of your template files in your <code>tailwind.config.js</code> file.
+      </p>
+    ),
+    code: {
+      name: 'tailwind.config.js',
+      lang: 'js',
+      code: `  /** @type {import('tailwindcss').Config} */
   module.exports = {
 >   content: [
 >     "./app/**/*.{js,ts,jsx,tsx}",
@@ -202,74 +57,59 @@ let tabs = [
     },
     plugins: [],
   }`,
-        },
-      },
-      {
-        title: 'Add the Tailwind directives to your CSS',
-        body: () => (
-          <p>
-            Add the <code>@tailwind</code> directives for each of Tailwind’s layers to your{' '}
-            <code>./app/globals.css</code> file.
-          </p>
-        ),
-        code: {
-          name: 'globals.css',
-          lang: 'css',
-          code: '@tailwind base;\n@tailwind components;\n@tailwind utilities;',
-        },
-      },
-      {
-        title: 'Start your build process',
-        body: () => (
-          <p>
-            Run your build process with <code>npm run dev</code>.
-          </p>
-        ),
-        code: {
-          name: 'Terminal',
-          lang: 'terminal',
-          code: 'npm run dev',
-        },
-      },
-      {
-        title: 'Start using Tailwind in your project',
-        body: () => <p>Start using Tailwind’s utility classes to style your content.</p>,
-        code: {
-          name: 'page.tsx',
-          lang: 'jsx',
-          code: `  export default function Home() {
+    },
+  },
+  {
+    title: 'Add the Tailwind directives to your CSS',
+    body: () => (
+      <p>
+        Add the <code>@tailwind</code> directives for each Tailwind’s layers to your{' '}
+        <code>globals.css</code> file.
+      </p>
+    ),
+    code: {
+      name: 'globals.css',
+      lang: 'css',
+      code: '@tailwind base;\n@tailwind components;\n@tailwind utilities;',
+    },
+  },
+  {
+    title: 'Start your build process',
+    body: () => (
+      <p>
+        Run your build process with <code>npm run dev</code>.
+      </p>
+    ),
+    code: {
+      name: 'Terminal',
+      lang: 'terminal',
+      code: 'npm run dev',
+    },
+  },
+  {
+    title: 'Start using Tailwind in your project',
+    body: () => <p>Start using Tailwind’s utility classes to style your content.</p>,
+    code: {
+      name: 'index.tsx',
+      lang: 'jsx',
+      code: `  export default function Home() {
     return (
 >     <h1 className="text-3xl font-bold underline">
 >       Hello world!
 >     </h1>
     )
   }`,
-        },
-      },
-    ],
+    },
   },
 ]
 
 export default function UsingNextJs({ code }) {
-  let hash = useRouteHash()
-
-  let selectedTabIndex = tabs.findIndex((tab) => tab.href === hash)
-
-  if (selectedTabIndex === -1) {
-    selectedTabIndex = 0
-  }
-
   return (
     <FrameworkGuideLayout
       title="Install Tailwind CSS with Next.js"
-      description="Setting up Tailwind CSS in a Next.js v10+ project."
+      description="Setting up Tailwind CSS in a Next.js project."
     >
-      <TabBar tabs={tabs} selectedTabIndex={selectedTabIndex} />
-      <Steps
-        intro={tabs[selectedTabIndex].intro}
-        steps={tabs[selectedTabIndex].steps}
-        code={code[selectedTabIndex]}
-      />
+      <Steps steps={steps} code={code} />
     </FrameworkGuideLayout>
   )
 }
@@ -279,7 +119,7 @@ export function getStaticProps() {
 
   return {
     props: {
-      code: tabs.map((tab) => highlightedCodeSnippets(tab.steps)),
+      code: highlightedCodeSnippets(steps),
     },
   }
 }
