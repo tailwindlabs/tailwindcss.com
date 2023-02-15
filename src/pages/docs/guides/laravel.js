f05b0d413ef3573774e9dd1fce30ea9ee0c58f2f
Ngô Quốc Đạt <datlechin@gmail.com>
2023-02-15 23:55:33 +0700

    update

--- src/pages/docs/guides/laravel.js
+++ src/pages/docs/guides/laravel.js
@@ -221,18 +221,18 @@ let tabs = [
           name: 'app.blade.php',
           lang: 'html',
           code: `  <!doctype html>
-    <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body>
->       <h1 class="text-3xl font-bold underline">
->           Hello world!
->       </h1>
-    </body>
-    </html>`,
+  <html>
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  </head>
+  <body>
+>     <h1 class="text-3xl font-bold underline">
+>         Hello world!
+>     </h1>
+  </body>
+  </html>`,
         },
       },
     ],
