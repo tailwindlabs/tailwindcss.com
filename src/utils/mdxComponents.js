ea3c6d21712c7a076ed4100bf56a9fa4358a58dc
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2023-03-27 12:11:40 -0400

    WIP

--- src/utils/mdxComponents.js
+++ src/utils/mdxComponents.js
@@ -1,8 +1,8 @@
 export const mdxComponents = {
   img: (props) => (
-    <div className="relative not-prose my-[2em] first:mt-0 last:mb-0 rounded-lg overflow-hidden">
+    <div className="relative not-prose my-[2em] first:mt-0 last:mb-0 rounded-2xl overflow-hidden">
       <img {...props} decoding="async" />
-      <div className="absolute inset-0 rounded-lg ring-1 ring-inset ring-slate-900/10" />
+      <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-slate-900/10" />
     </div>
   ),
 }
