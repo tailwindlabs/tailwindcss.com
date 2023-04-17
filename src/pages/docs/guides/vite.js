19448ee660e5acd13014b9693a8c3a5554aefcb0
maksple <maksple1337@gmail.com>
2023-04-17 11:50:48 +0200

    chore: code formatting

--- src/pages/docs/guides/vite.js
+++ src/pages/docs/guides/vite.js
@@ -86,18 +86,18 @@ let tabs = [
         code: {
           name: 'vite.config.js',
           lang: 'js',
-          code: `import { defineConfig } from "vite";
+          code: `  import { defineConfig } from "vite";
   import react from "@vitejs/plugin-react-swc";
 > import tailwindcss from "tailwindcss";
-  
+
   // https://vitejs.dev/config/
   export default defineConfig({
-    plugins: [react()],
->   css: {
->     postcss: {
->       plugins: [tailwindcss],
->     },
+  plugins: [react()],
+> css: {
+>   postcss: {
+>     plugins: [tailwindcss],
 >   },
+> },
   });`,
         },
       },
