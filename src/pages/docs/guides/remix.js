6bafed81efedf38a6db657aa19da5c648c0730e6
Robin Malfait <malfait.robin@gmail.com>
2023-05-04 15:02:36 +0200

    fix syntax issue

--- src/pages/docs/guides/remix.js
+++ src/pages/docs/guides/remix.js
@@ -60,7 +60,7 @@ let steps = [
       code: `  import type { Config } from 'tailwindcss'
 
   export default {
->   content: [''./app/**/*.{js,jsx,ts,tsx}''],
+>   content: ['./app/**/*.{js,jsx,ts,tsx}'],
     theme: {
       extend: {},
     },
