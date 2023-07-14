cc2becb4bbffd2d4b3278907806021b96d331628
lazizbeksultonov <lazizbeksultonov0804@gmail.com>
2023-07-14 11:11:45 +0500

    Main Page title translated

--- src/pages/index.js
+++ src/pages/index.js
@@ -88,7 +88,7 @@ function Header() {
         </div>
         <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
           <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
-            Rapidly build modern websites without ever leaving your HTML.
+            HTML-ni tark etmasdan tezda zamonaviy veb-saytlarni yarating.
           </h1>
           <p className="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
             A utility-first CSS framework packed with classes like{' '}
