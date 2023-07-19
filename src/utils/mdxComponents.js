973ee7641417a9baef9ac50be3286f4ca943b918
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2023-07-18 21:39:03 -0400

    WIP

--- src/utils/mdxComponents.js
+++ src/utils/mdxComponents.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 function Img(props) {
   return (
-    <div className="relative not-prose [a:not(:first-child)>&]:mt-[2em] [a:not(:last-child)>&]:mb-[2em] my-[2em] first:mt-0 last:mb-0 rounded-2xl overflow-hidden [figure>&]:my-0">
+    <div className="relative not-prose [a:not(:first-child)>&]:mt-12 [a:not(:last-child)>&]:mb-12 my-12 first:mt-0 last:mb-0 rounded-2xl overflow-hidden [figure>&]:my-0">
       <img {...props} decoding="async" />
       <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-slate-900/10 dark:ring-white/10" />
     </div>
