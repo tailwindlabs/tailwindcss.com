cabef7e5da8e41cb595a7255e635acde89555ea1
Rico <ricovzelst@pm.me>
2023-09-27 15:53:22 +0200

    Make including of vite resource more clear

--- src/pages/docs/guides/laravel.js
+++ src/pages/docs/guides/laravel.js
@@ -110,7 +110,7 @@ let tabs = [
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+>    @vite('resources/css/app.css')
   </head>
   <body>
 >   <h1 class="text-3xl font-bold underline">
