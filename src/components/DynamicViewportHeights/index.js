54bd9324bda7cc20fc88a6d45e969f90152052fa
Dan Hollick <dan.hollick@gmail.com>
2023-12-02 19:48:40 +0000

    Add examples

--- src/components/DynamicViewportHeights/index.js
+++ src/components/DynamicViewportHeights/index.js
@@ -1,51 +1,93 @@
-import React, { useState, useRef } from 'react'
-import { AnimatePresence, motion, useElementScroll, useMotionValue } from 'framer-motion'
-import PropTypes from 'prop-types'
+import React, { useState, useRef, useEffect } from 'react'
+import { motion, useElementScroll } from 'framer-motion'
+import clsx from 'clsx'
 
-MobileBrowser.propTypes = {}
+const viewport = {
+  lvh: 491,
+  svh: 443,
+  navBarHeight: 48,
+  width: 300,
+  padding: 5,
+}
 
-export function MobileBrowser(props) {
-  const parent = useRef(null)
-  const { scrollY } = useElementScroll(parent)
+const transition = {
+  duration: 0.4,
+}
+export function DynamicViewportExample({
+  unit = 'dvh',
+  colorStyles = 'dark:bg-blue-500 bg-blue-500 border border-blue-400',
+  containerBackground = 'bg-stripes-blue',
+}) {
   const [hidden, setHidden] = useState(false)
+  const parent = useRef(null)
 
-  useMotionValue(scrollY, 'change', (latest) => {
-    console.log(scrollY)
-    const previous = scrollY.getPrevious()
-    if (latest > previous) {
-      setHidden(true)
-    } else {
-      setHidden(false)
-    }
+  const { scrollY } = useElementScroll(parent)
+
+  useEffect(() => {
+    const unsub = scrollY.onChange((latest) => {
+      const previous = scrollY.getPrevious()
+      if (latest > previous) {
+        setHidden(true)
+      } else {
+        setHidden(false)
+      }
+    })
+
+    return () => unsub()
   })
-  const transition = {
-    duration: 0.4,
-  }
+
+  // NOTE:When upgrading Framer Motion we can use useMotionValueEvent instead:
+
+  // import {  motion, useScroll, useMotionValueEvent } from 'framer-motion'
+
+  // const { scrollY } = useScroll({ container: parent })
+
+  // useMotionValueEvent(scrollY, 'change', (latest) => {
+  //   const previous = scrollY.getPrevious()
+  //   if (latest > previous) {
+  //     setHidden(true)
+  //   } else {
+  //     setHidden(false)
+  //   }
+  // })
 
   return (
-    <div className="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25 p-8">
-      <div class="relative grid justify-items-center">
-        <motion.div
-          layout
-          ref={parent}
-          class="grid grid-rows-[auto_1fr] w-[320px] h-[524px] rounded-lg border border-slate-700 text-center text-xs text-white  overflow-y-scroll"
-        >
+    <>
+      <div className="not-prose mb-4">
+        <div className="flex space-x-2">
+          <svg
+            className="flex-none w-5 h-5"
+            viewBox="0 0 20 20"
+            fill="none"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            aria-hidden="true"
+          >
+            <path
+              d="m9.813 9.25.346-5.138a1.276 1.276 0 0 0-2.54-.235L6.75 11.25 5.147 9.327a1.605 1.605 0 0 0-2.388-.085.018.018 0 0 0-.004.019l1.98 4.87a5 5 0 0 0 4.631 3.119h3.885a4 4 0 0 0 4-4v-1a3 3 0 0 0-3-3H9.813Z"
+              className="stroke-slate-400 dark:stroke-slate-300"
+            />
+            <path
+              d="M3 5s.35-.47 1.25-.828m9.516-.422c2.078.593 3.484 1.5 3.484 1.5"
+              className="stroke-slate-400 dark:stroke-sky-400"
+            />
+          </svg>
+          <p className="text-slate-700 text-sm font-medium dark:text-slate-200">
+            Scroll the viewport to hide/show the browser UI
+          </p>
+        </div>
+      </div>
+      <div className="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25 md:px-8 py-8 px-4">
+        <div className="relative grid justify-items-center">
           <motion.div
             layout
-            variants={{
-              visible: {
-                height: '48px',
-              },
-              hidden: {
-                height: '0px',
-              },
-            }}
-            transition={{
-              ...transition,
-            }}
-            initial={'visible'}
-            animate={hidden ? 'hidden' : 'visible'}
-            class="overflow-clip"
+            ref={parent}
+            style={{ width: `${viewport.width}px`, height: `${viewport.lvh}px` }}
+            className={clsx(
+              containerBackground,
+              `relative grid grid-rows-[auto_1fr] rounded-lg border dark:border-slate-700 border-slate-300 text-center text-xs text-white overflow-y-scroll no-scrollbar `
+            )}
           >
             <motion.div
               layout
@@ -57,20 +99,20 @@ export function MobileBrowser(props) {
                   y: 0,
                 },
                 hidden: {
-                  y: '-100%',
+                  y: `-${viewport.navBarHeight}px`,
                 },
               }}
-              initial={'visible'}
+              initial="visible"
               animate={hidden ? 'hidden' : 'visible'}
-              class="grid grid-cols-[auto,1fr,auto] items-center justify-start gap-4 rounded-t-lg border-b border-slate-600 bg-slate-800 p-3 "
+              className="absolute w-full grid grid-cols-[auto,1fr,auto] items-center justify-start gap-4 rounded-t-lg border-b border-slate-300 dark:border-slate-600 dark:bg-slate-800 bg-slate-100 px-3 h-[48px]"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                strokeWidth="{1.5}"
+                strokeWidth="1.5"
                 stroke="currentColor"
-                class="h-5 w-5 text-slate-400"
+                className="h-5 w-5 text-slate-600 dark:text-slate-400"
               >
                 <path
                   strokeLinecap="round"
@@ -78,16 +120,16 @@ export function MobileBrowser(props) {
                   d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
                 />
               </svg>
-              <div class="w-full rounded-full border border-slate-700 bg-slate-700 px-4 py-1 text-slate-400">
+              <div className="w-full rounded-full border border-slate-200 dark:border-slate-700 bg-slate-50 dark:bg-slate-700 px-4 py-1  text-slate-600 dark:text-slate-400">
                 tailwindcss.com
               </div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                strokeWidth="{1.5}"
+                strokeWidth="1.5"
                 stroke="currentColor"
-                class="h-5 w-5 text-slate-400"
+                className="h-5 w-5 text-slate-600 dark:text-slate-400"
               >
                 <path
                   strokeLinecap="round"
@@ -96,36 +138,70 @@ export function MobileBrowser(props) {
                 />
               </svg>
             </motion.div>
+
+            <motion.div
+              key="modal"
+              layout
+              className="sticky w-full p-[5px] pointer-events-none"
+              transition={{
+                ...transition,
+              }}
+              variants={{
+                visible: {
+                  top: `${viewport.navBarHeight}px`,
+                  height: `${unit === 'lvh' ? viewport.lvh : viewport.svh}px`,
+                },
+                hidden: {
+                  top: '0px',
+                  height: `${unit === 'svh' ? viewport.svh : viewport.lvh}px`,
+                },
+              }}
+              initial="visible"
+              animate={hidden ? 'hidden' : 'visible'}
+            >
+              <div
+                className={clsx(
+                  colorStyles,
+                  'text-slate-50 font-mono font-bold w-full h-full rounded-md  grid items-center justify-center'
+                )}
+              >
+                h-{unit}
+              </div>
+            </motion.div>
+            <div style={{ height: `${viewport.navBarHeight}px` }}></div>
           </motion.div>
-          <motion.div
-            key="modal"
-            layout
-            class=" p-[5px] bg-stripes-blue text-slate-50 font-mono font-bold overflow-visible"
-          >
-            <div class="h-[600px] bg-blue-500 rounded-md  grid items-center justify-center border border-blue-400">
-              h-dvh
-            </div>
-          </motion.div>
-          {/* <motion.div
-            transition={{
-              repeat: Infinity,
-              ...transition,
-            }}
-            initial={{
-              height: '48px',
-            }}
-            animate={{
-              height: '0px',
-            }}
-            class="bg-blue-500 border-blue-400 border opacity-20 w-full max-w-[308px] mx-[5px] rounded-b-md absolute top-[522px] left-auto right-auto"
-          ></motion.div> */}
-        </motion.div>
+          {unit === 'lvh' && (
+            <motion.div
+              transition={{
+                ...transition,
+              }}
+              variants={{
+                visible: {
+                  bottom: `-${viewport.navBarHeight}px`,
+                  height: `${viewport.navBarHeight}px`,
+                },
+                hidden: {
+                  bottom: '5px',
+                  height: '0px',
+                },
+              }}
+              initial="visible"
+              animate={hidden ? 'hidden' : 'visible'}
+              // For some reason putting this in a tailwind class doesn't work
+              style={{ width: `${viewport.width - 12}px` }}
+              className={clsx(
+                colorStyles,
+                `opacity-20 rounded-b-md absolute left-0 right-0 mx-auto`
+              )}
+            ></motion.div>
+          )}
+        </div>
+        <div
+          style={{ backgroundPosition: '10px 10px' }}
+          className=" z-[-1] pointer-events-none absolute inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"
+        />
+        <div className="z-[-1] pointer-events-none absolute inset-0 border border-black/5 rounded-xl dark:border-white/5" />
       </div>
-      <div
-        style={{ backgroundPosition: '10px 10px' }}
-        className="  z-[-1] pointer-events-none absolute inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"
-      />
-      <div className=" z-[-1] pointer-events-none absolute inset-0 pointer-events-none border border-black/5 rounded-xl dark:border-white/5" />
-    </div>
+    </>
   )
 }
