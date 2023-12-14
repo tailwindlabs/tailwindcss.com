0f714496c8fb85e5d8f9ff1aa62f2ad959a8789c
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-12-15 00:26:36 +0300

    Update sveltekit.js

--- src/pages/docs/guides/sveltekit.js
+++ src/pages/docs/guides/sveltekit.js
@@ -47,7 +47,7 @@ let steps = [
       name: 'svelte.config.js',
       lang: 'js',
       code: `  import adapter from '@sveltejs/adapter-auto';
-> import { vitePreprocess } from '@sveltejs/kit/vite';
+> import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
   /** @type {import('@sveltejs/kit').Config} */
   const config = {
     kit: {
