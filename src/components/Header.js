c0d67e87c2f5ff5c75d2342b10ef33effd480887
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2023-12-21 11:17:35 -0500

    Update banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,10 +11,10 @@ import { ThemeSelect, ThemeToggle } from './ThemeToggle'
 function Featured() {
   return (
     <a
-      href="/blog/2023-07-18-tailwind-connect-2023-recap"
+      href="/blog/tailwindcss-v3-4"
       className="ml-3 text-xs leading-5 font-medium text-sky-600 dark:text-sky-400 bg-sky-400/10 rounded-full py-1 px-3 hidden xl:flex items-center hover:bg-sky-400/20"
     >
-      <strong className="font-semibold">Connect 2023 Recap</strong>
+      <strong className="font-semibold">Tailwind CSS v3.4</strong>
       <svg
         width="2"
         height="2"
@@ -24,7 +24,7 @@ function Featured() {
       >
         <circle cx="1" cy="1" r="1" />
       </svg>
-      <span className="ml-2">Everything that was announced at our special event</span>
+      <span className="ml-2">Dynamic viewport units, :has(), subgrid, and more</span>
       <svg
         width="3"
         height="6"
