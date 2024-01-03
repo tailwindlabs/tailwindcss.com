d960824a213a89c0c0c5a9d6e0b4e7302b0f05e5
Serkan <Serkanbay02@hotmail.com>
2024-01-03 11:41:38 +0300

    Update index.js
    "/dist/output.css" does not give the correct path for output.css.

--- src/pages/docs/installation/index.js
+++ src/pages/docs/installation/index.js
@@ -77,7 +77,7 @@ let steps = [
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
->   <link href="/dist/output.css" rel="stylesheet">
+>   <link href="../dist/output.css" rel="stylesheet">
   </head>
   <body>
 >   <h1 class="text-3xl font-bold underline">
