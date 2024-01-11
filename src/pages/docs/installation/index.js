31cea0f74c11c41bf9b4d312a811b6244716432f
Godnon Dsilva <godnondsilva@gmail.com>
2024-01-11 23:37:23 +0530

    docs(installation): update the path of the output.css

--- src/pages/docs/installation/index.js
+++ src/pages/docs/installation/index.js
@@ -77,7 +77,7 @@ let steps = [
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
->   <link href="/output.css" rel="stylesheet">
+>   <link href="./output.css" rel="stylesheet">
   </head>
   <body>
 >   <h1 class="text-3xl font-bold underline">
