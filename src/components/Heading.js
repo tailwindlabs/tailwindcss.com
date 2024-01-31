a7c82a7e2be838f6f59fb93beeca60fc609d50ae
CalvinMoylan <calvinmoylan1@gmail.com>
2024-02-01 01:52:27 +0800

    Fix blog heading hyperlink hover on dark mode (#1747)
    * Fixed blog heading hyperlink hover on dark mode
    
    * Removed accidental double space
    
    * More tweaks to heading hyperlink
    
    ---------
    
    Co-authored-by: Jonathan Reinink <jonathan@reinink.ca>

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -41,7 +41,12 @@ export function Heading({
       >
         <div className="absolute -ml-8 hidden items-center border-0 opacity-0 group-hover:opacity-100 group-focus:opacity-100 lg:flex">
           &#8203;
-          <div className="flex h-6 w-6 items-center justify-center rounded-md text-slate-400 shadow-sm ring-1 ring-slate-900/5 hover:text-slate-700 hover:shadow hover:ring-slate-900/10 dark:bg-slate-700 dark:text-slate-300 dark:shadow-none dark:ring-0">
+          <div
+            className={clsx(
+              'flex h-6 w-6 items-center justify-center rounded-md text-slate-400 shadow-sm ring-1 ring-slate-900/5 hover:text-slate-700 hover:shadow hover:ring-slate-900/10',
+              'dark:bg-slate-800 dark:text-slate-400 dark:shadow-none dark:ring-0 dark:hover:bg-slate-700 dark:hover:text-slate-200'
+            )}
+          >
             <svg width="12" height="12" fill="none" aria-hidden="true">
               <path
                 d="M3.75 1v10M8.25 1v10M1 3.75h10M1 8.25h10"
