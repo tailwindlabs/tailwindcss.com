f9d24c13243ead4883f4d9e43176c5b858b1544b
Romain Lanz <2793951+RomainLanz@users.noreply.github.com>
2024-03-09 17:02:29 +0100

    fix(adonisjs): we should have only one vite tag (#1787)

--- src/pages/docs/guides/adonisjs.js
+++ src/pages/docs/guides/adonisjs.js
@@ -98,8 +98,7 @@ let steps = [
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
->   @vite(['resources/css/app.css'])
-    @vite(['resources/js/app.js'])
+>   @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
   <body>
 >   <h1 class="text-3xl font-bold underline">
