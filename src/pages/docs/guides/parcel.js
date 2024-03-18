7bfc26888ecc2344a244de3cbbffd5efb4271d3b
Jonah Möller <jonah.moeller@googlemail.com>
2024-03-18 16:02:25 +0100

    add correct type to css file link in parcel example

--- src/pages/docs/guides/parcel.js
+++ src/pages/docs/guides/parcel.js
@@ -115,7 +115,7 @@ let steps = [
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
->   <link href="./index.css" rel="stylesheet">
+>   <link href="./index.css" type="text/css" rel="stylesheet">
   </head>
   <body>
 >   <h1 class="text-3xl font-bold underline">
