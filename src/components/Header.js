9402a67938a25b490557e5600c7640a0c7cb4441
Robin Malfait <malfait.robin@gmail.com>
2024-03-20 10:16:27 +0100

    fix featured URL

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ import { ThemeSelect, ThemeToggle } from './ThemeToggle'
 function Featured() {
   return (
     <a
-      href="/blog/tailwindcss-v3-4"
+      href="/blog/hiring-a-design-engineer-and-staff-engineer"
       className="ml-3 text-xs leading-5 font-medium text-sky-600 dark:text-sky-400 bg-sky-400/10 rounded-full py-1 px-3 hidden xl:flex items-center hover:bg-sky-400/20"
     >
       <strong className="font-semibold">We're hiring</strong>
