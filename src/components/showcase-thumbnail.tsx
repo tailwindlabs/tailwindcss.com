0e7736038bce1bb928a4ee1c600c216d1bca7165
Dan Hollick <dan.hollick@gmail.com>
2025-01-22 23:21:05 +0000

    fix showcase thumbnail link hit target

--- src/components/showcase-thumbnail.tsx
+++ src/components/showcase-thumbnail.tsx
@@ -67,7 +67,7 @@ export default function ShowcaseThumbnail({ showcase, priority = false }: { show
 
   return (
     <div
-      className="group dark:border-bg-white/10 border-x border-gray-950/5 transition-colors hover:bg-gray-950/5 max-sm:border-0 sm:max-lg:nth-[2n]:border-r-0 sm:max-lg:nth-[2n+1]:border-l-0 lg:max-xl:nth-[3n]:border-r-0 lg:max-xl:nth-[3n+1]:border-l-0 xl:nth-[4n]:border-r-0 xl:nth-[4n+1]:border-l-0"
+      className="group dark:border-bg-white/10 relative border-x border-gray-950/5 transition-colors hover:bg-gray-950/5 max-sm:border-0 sm:max-lg:nth-[2n]:border-r-0 sm:max-lg:nth-[2n+1]:border-l-0 lg:max-xl:nth-[3n]:border-r-0 lg:max-xl:nth-[3n+1]:border-l-0 xl:nth-[4n]:border-r-0 xl:nth-[4n+1]:border-l-0"
       onMouseEnter={() => {
         if (state.current === "idle") {
           state.current = "playing";
@@ -82,6 +82,7 @@ export default function ShowcaseThumbnail({ showcase, priority = false }: { show
         hideVideo();
       }}
     >
+      <a href={showcase.url} target="_blank" rel="noreferrer" className="absolute inset-0 z-10"></a>
       <GridContainer className={clsx("p-2", BEFORE_AND_AFTER_ONLY_IN_FIRST_COLUMN_OF_CURRENT_GRID)}>
         <div className="relative aspect-[672/494] overflow-hidden rounded-xl outline outline-gray-950/5">
           <Image
@@ -145,11 +146,7 @@ export default function ShowcaseThumbnail({ showcase, priority = false }: { show
       </GridContainer>
       <GridContainer className={clsx("mt-2 p-2", BEFORE_AND_AFTER_ONLY_IN_FIRST_COLUMN_OF_CURRENT_GRID)}>
         <div className="flex flex-wrap items-center">
-          <h2 className="text-sm/6 font-medium">
-            <a href={showcase.url} target="_blank" rel="noreferrer">
-              {showcase.name}
-            </a>
-          </h2>
+          <h2 className="text-sm/6 font-medium">{showcase.name}</h2>
           {showcase.isTemplate && (
             <p
               aria-label="This site is a Tailwind UI template"
