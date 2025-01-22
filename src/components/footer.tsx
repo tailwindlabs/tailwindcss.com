41e749eb7ec4799def14430f64438bae40fb2488
Robin Malfait <malfait.robin@gmail.com>
2025-01-22 13:56:33 -0500

    improve footer layout on mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,101 +6,41 @@ import clsx from "clsx";
 export function FooterSitemap({ className }: { className?: string }) {
   return (
     <footer className="bg-white text-sm/loose text-gray-950 dark:bg-gray-950 dark:text-white">
+      <div className={clsx("flex gap-4 p-4 md:hidden", className)}>
+        <div className="flex flex-1 flex-col gap-10">
+          <div>
+            <Learn />
+          </div>
+          <div>
+            <Resources />
+          </div>
+        </div>
+        <div className="flex flex-1 flex-col gap-10">
+          <div>
+            <TailwindUI />
+          </div>
+          <div>
+            <Community />
+          </div>
+        </div>
+      </div>
       <div
         className={clsx(
-          "mx-auto grid w-full grid-cols-2 justify-between gap-y-0 md:grid-cols-4 md:gap-6 md:gap-x-4 lg:gap-8",
+          "mx-auto hidden w-full grid-cols-4 justify-between gap-y-0 md:grid md:grid-cols-4 md:gap-6 md:gap-x-4 lg:gap-8",
           className,
         )}
       >
         <div className="border-x border-b border-gray-950/5 py-10 pl-2 not-md:border-0 md:border-b-0 dark:border-white/10">
-          <h3 className="font-semibold">Learn</h3>
-          <ul className="mt-4 grid gap-4">
-            <li>
-              <Link href="/docs" className="hover:underline">
-                Documentation
-              </Link>
-            </li>
-            <li>
-              <Link href="/showcase" className="hover:underline">
-                Showcase
-              </Link>
-            </li>
-            <li>
-              <Link href="/blog" className="hover:underline">
-                Blog
-              </Link>
-            </li>
-            <li>
-              <Link href="https://play.tailwindcss.com/" className="hover:underline">
-                Playground
-              </Link>
-            </li>
-          </ul>
+          <Learn />
         </div>
         <div className="border-x border-b border-gray-950/5 py-10 pl-2 not-md:border-0 md:border-b-0 dark:border-white/10">
-          <h3 className="mb-2 font-semibold">Tailwind UI</h3>
-          <ul className="mt-4 grid gap-4">
-            <li>
-              <Link href="https://tailwindui.com/components?ref=footer" className="hover:underline">
-                UI Blocks
-              </Link>
-            </li>
-            <li>
-              <Link href="https://tailwindui.com/templates?ref=footer" className="hover:underline">
-                Templates
-              </Link>
-            </li>
-            <li>
-              <Link href="https://tailwindui.com/templates/catalyst?ref=footer" className="hover:underline">
-                UI Kit
-              </Link>
-            </li>
-          </ul>
+          <TailwindUI />
         </div>
         <div className="border-x border-b border-gray-950/5 py-10 pl-2 not-md:border-0 sm:border-b-0 dark:border-white/10">
-          <h3 className="font-semibold">Resources</h3>
-          <ul className="mt-4 grid gap-4">
-            <li>
-              <Link href="https://www.refactoringui.com" className="hover:underline">
-                Refactoring UI
-              </Link>
-            </li>
-            <li>
-              <Link href="https://headlessui.com" className="hover:underline">
-                Headless UI
-              </Link>
-            </li>
-            <li>
-              <Link href="https://heroicons.com" className="hover:underline">
-                Heroicons
-              </Link>
-            </li>
-            <li>
-              <Link href="https://heropatterns.com" className="hover:underline">
-                Hero Patterns
-              </Link>
-            </li>
-          </ul>
+          <Resources />
         </div>
         <div className="border-x border-gray-950/5 py-10 pl-2 not-md:border-0 dark:border-white/10">
-          <h3 className="font-semibold">Community</h3>
-          <ul className="mt-4 grid gap-4">
-            <li>
-              <Link href="https://github.com/tailwindlabs/tailwindcss" className="hover:underline">
-                GitHub
-              </Link>
-            </li>
-            <li>
-              <Link href="https://tailwindcss.com/discord" className="hover:underline">
-                Discord
-              </Link>
-            </li>
-            <li>
-              <Link href="https://x.com/tailwindcss" className="hover:underline">
-                X
-              </Link>
-            </li>
-          </ul>
+          <Community />
         </div>
       </div>
     </footer>
@@ -126,5 +66,115 @@ export function FooterMeta({ className }: { className?: string }) {
         </div>
       </div>
     </div>
+  );
+}
+
+function Learn() {
+  return (
+    <>
+      <h3 className="font-semibold">Learn</h3>
+      <ul className="mt-4 grid gap-4">
+        <li>
+          <Link href="/docs" className="hover:underline">
+            Documentation
+          </Link>
+        </li>
+        <li>
+          <Link href="/showcase" className="hover:underline">
+            Showcase
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog" className="hover:underline">
+            Blog
+          </Link>
+        </li>
+        <li>
+          <Link href="https://play.tailwindcss.com/" className="hover:underline">
+            Playground
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+function TailwindUI() {
+  return (
+    <>
+      <h3 className="mb-2 font-semibold">Tailwind UI</h3>
+      <ul className="mt-4 grid gap-4">
+        <li>
+          <Link href="https://tailwindui.com/components?ref=footer" className="hover:underline">
+            UI Blocks
+          </Link>
+        </li>
+        <li>
+          <Link href="https://tailwindui.com/templates?ref=footer" className="hover:underline">
+            Templates
+          </Link>
+        </li>
+        <li>
+          <Link href="https://tailwindui.com/templates/catalyst?ref=footer" className="hover:underline">
+            UI Kit
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+function Resources() {
+  return (
+    <>
+      <h3 className="font-semibold">Resources</h3>
+      <ul className="mt-4 grid gap-4">
+        <li>
+          <Link href="https://www.refactoringui.com" className="hover:underline">
+            Refactoring UI
+          </Link>
+        </li>
+        <li>
+          <Link href="https://headlessui.com" className="hover:underline">
+            Headless UI
+          </Link>
+        </li>
+        <li>
+          <Link href="https://heroicons.com" className="hover:underline">
+            Heroicons
+          </Link>
+        </li>
+        <li>
+          <Link href="https://heropatterns.com" className="hover:underline">
+            Hero Patterns
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+function Community() {
+  return (
+    <>
+      <h3 className="font-semibold">Community</h3>
+      <ul className="mt-4 grid gap-4">
+        <li>
+          <Link href="https://github.com/tailwindlabs/tailwindcss" className="hover:underline">
+            GitHub
+          </Link>
+        </li>
+        <li>
+          <Link href="https://tailwindcss.com/discord" className="hover:underline">
+            Discord
+          </Link>
+        </li>
+        <li>
+          <Link href="https://x.com/tailwindcss" className="hover:underline">
+            X
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 }
