1087c59f5d5dd5908e9005c171b3723bba7b0269
Jonathan Reinink <jonathan@reinink.ca>
2025-01-22 15:31:46 -0500

    Tweak 404 styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -17,7 +17,7 @@ export default function NotFoundPage() {
               <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:mr-6 sm:border-r sm:border-gray-900/10 sm:pr-6 sm:text-3xl dark:text-gray-200 sm:dark:border-gray-300/10">
                 404
               </h1>
-              <h2 className="mt-2 text-lg text-gray-700 sm:mt-0 dark:text-gray-400">This page could not be found.</h2>
+              <h2 className="mt-2 text-gray-700 sm:mt-0 dark:text-gray-400">This page could not be found.</h2>
             </div>
           </div>
         </div>
