8f0020f676492a2acbe924f49a54bd9e7c12dd43
Philipp Spiess <hello@philippspiess.com>
2025-01-22 11:54:04 -0500

    Tweaks

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -59,7 +59,7 @@ const Hero: React.FC = () => {
         <span className="max-sm:hidden">text-balance</span>
       </div>
       <GridContainer>
-        <h1 className="px-2 text-4xl tracking-tighter text-balance max-sm:px-4 sm:text-5xl lg:text-6xl xl:text-8xl">
+        <h1 className="px-2 text-4xl tracking-tighter text-balance max-lg:font-medium max-sm:px-4 sm:text-5xl lg:text-6xl xl:text-8xl">
           Rapidly build modern websites without ever leaving your HTML.
         </h1>
       </GridContainer>
