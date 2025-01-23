d95c993dc06e18a2eebdde1cb6706670b591a000
Ryan Huellen <me@ryanhuellen.com>
2025-01-22 22:25:05 -0600

    style: spaces instead of tabs

--- src/components/color-palette.tsx
+++ src/components/color-palette.tsx
@@ -9,58 +9,58 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const styles = await fs.readFile(
-	path.join(__dirname, "../../node_modules/tailwindcss/theme.css"),
-	"utf-8",
+  path.join(__dirname, "../../node_modules/tailwindcss/theme.css"),
+  "utf-8",
 );
 
 const colors: Record<string, Record<string, string>> = {};
 for (const line of styles.split("\n")) {
-	if (line.startsWith("  --color-")) {
-		const [key, value] = line
-			.split(":")
-			.map((part) => part.trim().replace(";", ""));
-		const match = key.match(/^--color-([a-z]+)-(\d+)$/);
+  if (line.startsWith("  --color-")) {
+    const [key, value] = line
+      .split(":")
+      .map((part) => part.trim().replace(";", ""));
+    const match = key.match(/^--color-([a-z]+)-(\d+)$/);
 
-		if (match) {
-			const [, group, shade] = match;
+    if (match) {
+      const [, group, shade] = match;
 
-			if (!colors[group]) {
-				colors[group] = {};
-			}
+      if (!colors[group]) {
+        colors[group] = {};
+      }
 
-			colors[group][shade] = value;
-		}
-	}
+      colors[group][shade] = value;
+    }
+  }
 }
 
 export function ColorPalette() {
-	return (
-		<div className="not-prose grid grid-cols-[auto_minmax(0,_1fr)] items-center gap-4">
-			<div className="col-start-2 grid grid-cols-11 justify-items-center gap-1.5 font-medium text-gray-950 *:rotate-180 *:[writing-mode:vertical-lr] sm:gap-4 sm:*:rotate-0 sm:*:[writing-mode:horizontal-tb] dark:text-white">
-				<div>50</div>
-				<div>100</div>
-				<div>200</div>
-				<div>300</div>
-				<div>400</div>
-				<div>500</div>
-				<div>600</div>
-				<div>700</div>
-				<div>800</div>
-				<div>900</div>
-				<div>950</div>
-			</div>
-			{Object.entries(colors).map(([key, shades]) => (
-				<React.Fragment key={key}>
-					<p className="font-medium text-gray-950 capitalize sm:pr-12 dark:text-white">
-						{key}
-					</p>
-					<div className="grid grid-cols-11 gap-1.5 sm:gap-4">
-						{Object.keys(shades).map((shade, i) => (
-							<Color key={i} name={key} shade={shade} />
-						))}
-					</div>
-				</React.Fragment>
-			))}
-		</div>
-	);
+  return (
+    <div className="not-prose grid grid-cols-[auto_minmax(0,_1fr)] items-center gap-4">
+      <div className="col-start-2 grid grid-cols-11 justify-items-center gap-1.5 font-medium text-gray-950 *:rotate-180 *:[writing-mode:vertical-lr] sm:gap-4 sm:*:rotate-0 sm:*:[writing-mode:horizontal-tb] dark:text-white">
+        <div>50</div>
+        <div>100</div>
+        <div>200</div>
+        <div>300</div>
+        <div>400</div>
+        <div>500</div>
+        <div>600</div>
+        <div>700</div>
+        <div>800</div>
+        <div>900</div>
+        <div>950</div>
+      </div>
+      {Object.entries(colors).map(([key, shades]) => (
+        <React.Fragment key={key}>
+          <p className="font-medium text-gray-950 capitalize sm:pr-12 dark:text-white">
+            {key}
+          </p>
+          <div className="grid grid-cols-11 gap-1.5 sm:gap-4">
+            {Object.keys(shades).map((shade, i) => (
+              <Color key={i} name={key} shade={shade} />
+            ))}
+          </div>
+        </React.Fragment>
+      ))}
+    </div>
+  );
 }
--- src/components/color.tsx
+++ src/components/color.tsx
@@ -4,83 +4,83 @@ import { oklch2hex } from "colorizr";
 import { useEffect, useRef, useState } from "react";
 import clsx from "clsx";
 import {
-	Button,
-	Tooltip,
-	TooltipPanel,
-	TooltipTrigger,
+  Button,
+  Tooltip,
+  TooltipPanel,
+  TooltipTrigger,
 } from "@headlessui/react";
 
 export function Color({ name, shade }: { name: string; shade: string }) {
-	const [justCopied, setJustCopied] = useState(false);
+  const [justCopied, setJustCopied] = useState(false);
 
-	const buttonRef = useRef<HTMLButtonElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
-	const colorVariableName = `--color-${name}-${shade}`;
+  const colorVariableName = `--color-${name}-${shade}`;
 
-	const copyHexToClipboard = () => {
-		if (!buttonRef.current) {
-			return;
-		}
+  const copyHexToClipboard = () => {
+    if (!buttonRef.current) {
+      return;
+    }
 
-		const styleValue = buttonRef.current
-			.computedStyleMap()
-			.get(colorVariableName);
+    const styleValue = buttonRef.current
+      .computedStyleMap()
+      .get(colorVariableName);
 
-		if (!styleValue) {
-			return;
-		}
+    if (!styleValue) {
+      return;
+    }
 
-		const oklchWithCSSFunctionalNotation = styleValue.toString();
+    const oklchWithCSSFunctionalNotation = styleValue.toString();
 
-		// oklch(0.808 0.114 19.571) to 0.808 0.114 19.571
-		const oklch = oklchWithCSSFunctionalNotation.slice(6, -1);
+    // oklch(0.808 0.114 19.571) to 0.808 0.114 19.571
+    const oklch = oklchWithCSSFunctionalNotation.slice(6, -1);
 
-		// 0.808 0.114 19.571 to [0.808, 0.114, 19.571]
-		const oklchTuple = oklch.split(" ").map(Number) as [number, number, number];
+    // 0.808 0.114 19.571 to [0.808, 0.114, 19.571]
+    const oklchTuple = oklch.split(" ").map(Number) as [number, number, number];
 
-		const hex = oklch2hex(oklchTuple);
+    const hex = oklch2hex(oklchTuple);
 
-		navigator.clipboard.writeText(hex);
+    navigator.clipboard.writeText(hex);
 
-		setJustCopied(true);
-	};
+    setJustCopied(true);
+  };
 
-	useEffect(() => {
-		const timeout = setTimeout(() => {
-			if (!justCopied) {
-				return;
-			}
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (!justCopied) {
+        return;
+      }
 
-			setJustCopied(false);
-		}, 1300);
+      setJustCopied(false);
+    }, 1300);
 
-		return () => clearTimeout(timeout);
-	}, [justCopied]);
+    return () => clearTimeout(timeout);
+  }, [justCopied]);
 
-	return (
-		<Tooltip as="div" showDelayMs={100} hideDelayMs={0} className="relative">
-			<TooltipTrigger>
-				<Button
-					ref={buttonRef}
-					type="button"
-					onClick={copyHexToClipboard}
-					onTouchEnd={copyHexToClipboard}
-					style={{ backgroundColor: `var(${colorVariableName})` }}
-					className={clsx(
-						"h-full w-full cursor-pointer aspect-1/1 rounded-sm outline -outline-offset-1 outline-black/10 sm:rounded-md dark:outline-white/10 flex items-center justify-center",
-					)}
-				/>
-			</TooltipTrigger>
-			<TooltipPanel
-				as="div"
-				anchor="top"
-				className="pointer-events-none z-10 translate-y-2 rounded-full border border-gray-950 bg-gray-950/90 py-0.5 pr-2 pb-1 pl-3 text-center font-mono text-xs/6 font-medium whitespace-nowrap text-white opacity-100 inset-ring inset-ring-white/10 transition-[opacity] starting:opacity-0"
-			>
-				{justCopied ? "Copied!" : "Click to copy"}
-			</TooltipPanel>
-		</Tooltip>
-	);
-	/*
-		
-		*/
+  return (
+    <Tooltip as="div" showDelayMs={100} hideDelayMs={0} className="relative">
+      <TooltipTrigger>
+        <Button
+          ref={buttonRef}
+          type="button"
+          onClick={copyHexToClipboard}
+          onTouchEnd={copyHexToClipboard}
+          style={{ backgroundColor: `var(${colorVariableName})` }}
+          className={clsx(
+            "h-full w-full cursor-pointer aspect-1/1 rounded-sm outline -outline-offset-1 outline-black/10 sm:rounded-md dark:outline-white/10 flex items-center justify-center",
+          )}
+        />
+      </TooltipTrigger>
+      <TooltipPanel
+        as="div"
+        anchor="top"
+        className="pointer-events-none z-10 translate-y-2 rounded-full border border-gray-950 bg-gray-950/90 py-0.5 pr-2 pb-1 pl-3 text-center font-mono text-xs/6 font-medium whitespace-nowrap text-white opacity-100 inset-ring inset-ring-white/10 transition-[opacity] starting:opacity-0"
+      >
+        {justCopied ? "Copied!" : "Click to copy"}
+      </TooltipPanel>
+    </Tooltip>
+  );
+  /*
+  
+  */
 }
