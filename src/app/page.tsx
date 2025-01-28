c5a1c42ed221b3477009457cf37d1f2f727b6b0a
Robin Malfait <malfait.robin@gmail.com>
2025-01-28 13:25:55 +0100

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
           <FooterMeta className="px-4 md:px-6 lg:px-8" />
         </div>
       </div>
-      <div className="!fixed" aria-hidden="true">
+      <div className="fixed!" aria-hidden="true">
         <input type="text" tabIndex={-1} />
       </div>
     </div>
