74b8da62b94cbf108e41cced5cfed9b29e509ed1
Halah <Halah.m@bootfi.com>
2025-02-26 12:47:25 +0300

    add overflow auto to fix show more expand list
    add overflow-y-auto to fix show more expand list when it overflow the content over the footer

--- src/app/(docs)/layout.tsx
+++ src/app/(docs)/layout.tsx
@@ -18,7 +18,7 @@ export default async function Layout({
           <DocsSidebar />
         </MobileNav>
       </div>
-      <div className="grid min-h-dvh grid-cols-1 grid-rows-[1fr_1px_auto_1px_auto] pt-26.25 lg:grid-cols-[var(--container-2xs)_2.5rem_minmax(0,1fr)_2.5rem] lg:pt-14.25 xl:grid-cols-[var(--container-2xs)_2.5rem_minmax(0,1fr)_2.5rem]">
+      <div className="grid min-h-dvh grid-cols-1 grid-rows-[1fr_1px_auto_1px_auto] pt-26.25 lg:grid-cols-[var(--container-2xs)_2.5rem_minmax(0,1fr)_2.5rem] lg:pt-14.25 xl:grid-cols-[var(--container-2xs)_2.5rem_minmax(0,1fr)_2.5rem] overflow-y-auto">
         {/* Sidebar */}
         <div className="relative col-start-1 row-span-full row-start-1 max-lg:hidden">
           <div className="absolute inset-0">
