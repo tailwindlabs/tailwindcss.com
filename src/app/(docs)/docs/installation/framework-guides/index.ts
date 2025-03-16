da316d2ca330b0b4b5d0825dd5158abbb110c5fb
dwkns <dwkns@me.com>
2025-03-16 12:46:39 +0000

    Add Eleventy Framework guide

--- src/app/(docs)/docs/installation/framework-guides/index.ts
+++ src/app/(docs)/docs/installation/framework-guides/index.ts
@@ -26,6 +26,7 @@ const guides: Guide[] = await create({
   astro: () => import("./astro"),
   qwik: () => import("./qwik"),
   rspack: () => import("./rspack"),
+  eleventy: () => import("./eleventy"),
 });
 
 async function create(list: Record<string, () => Promise<any>>): Promise<Guide[]> {
