08114baf9bdc2841cdfad246d9f40a35f721e9fe
Daniel Hollick <danhollick@users.noreply.github.com>
2025-03-28 19:46:20 +0000

    Update `background-position` and `background-size` docs (#2180)
    Changes the api from `bg-left-top` to `bg-top-left` etc
    
    ---------
    
    Co-authored-by: Jonathan Reinink <jonathan@reinink.ca>

--- src/components/stripes.tsx
+++ src/components/stripes.tsx
@@ -16,7 +16,7 @@ export function Stripes({
         border === "x" && "border-x",
         border === "y" && "border-y",
         noColor === false && "text-black/10 dark:text-white/12.5",
-        "bg-[size:8px_8px] bg-left-top",
+        "bg-[size:8px_8px] bg-top-left",
         "bg-[image:repeating-linear-gradient(315deg,currentColor_0,currentColor_1px,transparent_0,transparent_50%)]",
       )}
     >
