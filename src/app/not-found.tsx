58b1d1a8cafdf9a7336e038d0a375b02ad3108ff
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2025-05-26 13:49:41 +0200

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -16,7 +16,7 @@ export default function NotFoundPage() {
 
         {/* Main content area */}
         <div className="text-gray-950 dark:text-white">
-          <div className="flex h-[calc(100dvh-var(--spacing)*14.25-2px)] items-center justify-center">
+          <div className="flex h-full items-center justify-center">
             <div className="flex flex-auto flex-col items-center justify-center px-4 text-center sm:flex-row">
               <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:mr-6 sm:border-r sm:border-gray-900/10 sm:pr-6 sm:text-3xl dark:text-gray-200 sm:dark:border-gray-300/10">
                 404
