edec3b1d24a8904b93b8f46abffa3c368d02731d
Jordan Pittman <jordan@cryptica.me>
2025-06-12 10:46:11 -0400

    change wording

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -28,10 +28,10 @@ export default async function Showcase() {
 
       <GridContainer className="mt-10">
         <p className="prose mx-2 max-w-(--breakpoint-md) text-lg leading-8 text-gray-600 dark:text-gray-400">
-          Well not quite <em>anything</em>, like you can't build a Mars rover with it. But you can definitely build the
-          website for the rover —{" "}
+          Well not quite <em>anything</em>, like you can't build a spaceship with it. But you can definitely build the
+          website for the spaceship —{" "}
           <a href="https://www.jpl.nasa.gov/" target="_blank" rel="noreferrer">
-            NASA/JPL
+            NASA
           </a>{" "}
           did.
         </p>
