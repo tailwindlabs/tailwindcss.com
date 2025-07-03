77d40a840879afdfc1b763f63b4371ad242ecd8b
Jonathan Reinink <jonathan@reinink.ca>
2025-07-03 14:59:28 -0400

    Fix sponsor link

--- src/components/home/partners-section.tsx
+++ src/components/home/partners-section.tsx
@@ -42,7 +42,7 @@ export default function WhyTailwindCssSection() {
       <GridContainer className="mt-10">
         <div className="px-2 max-sm:px-4">
           <Link
-            href="https://tailwindcss.com/plus?ref=home"
+            href="/sponsors"
             className="inline-flex items-center justify-center gap-2 rounded-4xl bg-black px-4 py-2 text-sm/6 font-semibold text-white hover:bg-gray-800 dark:bg-gray-700 dark:hover:bg-gray-600"
           >
             Become a sponsor
