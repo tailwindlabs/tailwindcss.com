2db2be7bcbe25c93cce43b63b52b2fad745da736
Jonathan Reinink <jonathan@reinink.ca>
2025-07-03 14:58:27 -0400

    Add link to sponsors section on home page

--- src/components/home/partners-section.tsx
+++ src/components/home/partners-section.tsx
@@ -1,6 +1,7 @@
+import { partners } from "@/app/sponsor/page";
+import Link from "next/link";
 import GridContainer from "../grid-container";
 import CategoryHeader from "./category-header";
-import { partners } from "@/app/sponsor/page";
 
 export default function WhyTailwindCssSection() {
   return (
@@ -36,6 +37,20 @@ export default function WhyTailwindCssSection() {
           Tailwind is supported by incredible partners and sponsors who make it possible for a team of talented
           designers and engineers to maintain the framework full-time.
         </p>
+      </GridContainer>
+
+      <GridContainer className="mt-10">
+        <div className="px-2 max-sm:px-4">
+          <Link
+            href="https://tailwindcss.com/plus?ref=home"
+            className="inline-flex items-center justify-center gap-2 rounded-4xl bg-black px-4 py-2 text-sm/6 font-semibold text-white hover:bg-gray-800 dark:bg-gray-700 dark:hover:bg-gray-600"
+          >
+            Become a sponsor
+            <svg fill="currentColor" aria-hidden="true" viewBox="0 0 10 10" className="-mr-0.5 w-2.5">
+              <path d="M4.85355 0.146423L9.70711 4.99998L4.85355 9.85353L4.14645 9.14642L7.79289 5.49998H0V4.49998H7.79289L4.14645 0.85353L4.85355 0.146423Z"></path>
+            </svg>
+          </Link>
+        </div>
       </GridContainer>
 
       <section>
