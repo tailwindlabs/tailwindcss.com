deeb7c09469d2d7635461c4399a4bc981d72dca1
Jordan Pittman <jordan@cryptica.me>
2025-07-08 16:17:25 -0400

    Add copy button to multi cursor animation
    Is this really necessary? No. But it is fun :D

--- src/components/multi-cursor/example.tsx
+++ src/components/multi-cursor/example.tsx
@@ -2,7 +2,8 @@ import clsx from "clsx";
 import { ThemedToken } from "shiki";
 import { html } from "../code-example";
 import { HighlightedCode } from "../highlight";
-import { TypingAnimation } from "./animation";
+import { AnimationCopyButton, TypingAnimation } from "./animation";
+import { stripShikiComments } from "../shiki";
 
 export function MultiCursorCode() {
   let code = html`
@@ -24,62 +25,67 @@ export function MultiCursorCode() {
   `;
 
   return (
-    <HighlightedCode
-      className={clsx(
-        "*:flex *:*:max-w-none *:*:shrink-0 *:*:grow *:overflow-auto *:rounded-lg *:bg-white/10! *:p-5 *:inset-ring *:inset-ring-white/10 dark:*:bg-white/5! dark:*:inset-ring-white/5",
-        "**:[.line]:isolate **:[.line]:block **:[.line]:not-last:min-h-[1lh]",
-      )}
-      example={code}
-      transformers={[
-        {
-          tokens(tokens) {
-            let count = 0;
+    <>
+      <div className="relative">
+        <AnimationCopyButton code={stripShikiComments(code.code)} />
+      </div>
+      <HighlightedCode
+        className={clsx(
+          "*:flex *:*:max-w-none *:*:shrink-0 *:*:grow *:overflow-auto *:rounded-lg *:bg-white/10! *:p-5 *:inset-ring *:inset-ring-white/10 dark:*:bg-white/5! dark:*:inset-ring-white/5",
+          "**:[.line]:isolate **:[.line]:block **:[.line]:not-last:min-h-[1lh]",
+        )}
+        example={code}
+        transformers={[
+          {
+            tokens(tokens) {
+              let count = 0;
 
-            return tokens.map((line, idx) => {
-              let tokens: ThemedToken[] = [];
+              return tokens.map((line, idx) => {
+                let tokens: ThemedToken[] = [];
 
-              for (let token of line) {
-                let classIndex = token.content.indexOf("__CLASS__");
+                for (let token of line) {
+                  let classIndex = token.content.indexOf("__CLASS__");
 
-                if (classIndex === -1) {
-                  tokens.push(token);
-                  continue;
+                  if (classIndex === -1) {
+                    tokens.push(token);
+                    continue;
+                  }
+
+                  let before = token.content.slice(0, classIndex);
+                  let after = token.content.slice(classIndex + 9);
+
+                  if (before) {
+                    tokens.push({ ...token, content: before });
+                  }
+
+                  tokens.push({
+                    ...token,
+                    content: "__CLASS__",
+                    htmlAttrs: {
+                      index: `${count++}`,
+                    },
+                  });
+
+                  if (after) {
+                    tokens.push({ ...token, content: after });
+                  }
                 }
 
-                let before = token.content.slice(0, classIndex);
-                let after = token.content.slice(classIndex + 9);
-
-                if (before) {
-                  tokens.push({ ...token, content: before });
-                }
-
-                tokens.push({
-                  ...token,
-                  content: "__CLASS__",
-                  htmlAttrs: {
-                    index: `${count++}`,
-                  },
-                });
-
-                if (after) {
-                  tokens.push({ ...token, content: after });
-                }
-              }
-
-              return tokens;
-            });
+                return tokens;
+              });
+            },
           },
-        },
-      ]}
-      components={{
-        span: (props) => {
-          if (props.children === "__CLASS__") {
-            return <TypingAnimation {...props} index={Number(props.index)} />;
-          }
+        ]}
+        components={{
+          span: (props) => {
+            if (props.children === "__CLASS__") {
+              return <TypingAnimation {...props} index={Number(props.index)} />;
+            }
 
-          return <span {...props} />;
-        },
-      }}
-    />
+            return <span {...props} />;
+          },
+        }}
+      />
+    </>
   );
 }
