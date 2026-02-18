7ce2710f60759d61b5281c2dc6f4e18abb9b7bbc
Robin Malfait <malfait.robin@gmail.com>
2026-02-18 16:47:11 +0100

    small canonicalization improvement

--- src/components/color-palette.tsx
+++ src/components/color-palette.tsx
@@ -36,7 +36,7 @@ export function ColorPalette() {
         className="pointer-events-none absolute top-0 left-0 z-10 rounded-full border border-gray-950 bg-gray-950/90 py-0.5 pr-2 pb-1 pl-3 text-center font-mono text-xs/6 font-medium whitespace-nowrap text-white opacity-0 inset-ring inset-ring-white/10 will-change-[transform,opacity] data-show:opacity-100 data-show:transition-opacity data-show:delay-100 data-show:duration-200"
       />
 
-      <div className="not-prose grid grid-cols-[auto_minmax(0,_1fr)] items-center gap-4">
+      <div className="not-prose grid grid-cols-[auto_minmax(0,1fr)] items-center gap-4">
         <div className="sticky top-28 z-9 bg-white lg:top-14 dark:bg-gray-950">&nbsp;</div>
         <div className="sticky top-28 z-9 col-start-2 grid grid-cols-11 justify-items-center gap-1.5 bg-white font-medium text-gray-950 *:rotate-180 *:[writing-mode:vertical-lr] max-sm:py-1 sm:gap-4 sm:*:rotate-0 sm:*:[writing-mode:horizontal-tb] lg:top-14 dark:bg-gray-950 dark:text-white">
           <div>50</div>
