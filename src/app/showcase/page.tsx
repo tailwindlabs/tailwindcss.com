292a9e7eb72f8cc2c1eb56dc976bbd8beb5dc4cf
Peter Suhm <peter@suhm.dk>
2026-03-24 12:32:19 +0100

    update sponsor page

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -81,13 +81,6 @@ const showcase = [
     description: "Developer tool website",
   },
   {
-    name: "Open",
-    url: "https://www.open.cx/?utm_source=tailwindcss",
-    thumbnail: require("./img/open.cx.png").default,
-    video: "/showcase-videos/open.cx.mp4",
-    description: "SaaS application website",
-  },
-  {
     name: "Vercel",
     url: "https://vercel.com/?utm_source=tailwindcss",
     thumbnail: require("./img/vercel.com.png").default,
